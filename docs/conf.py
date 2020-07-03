5131d4e75e96612283304357db856e3a16f1a31c
nanmu42 <i@nanmu.me>
2020-07-03 19:17:02 +0800

    doc: do not skip __init__

--- docs/conf.py
+++ docs/conf.py
@@ -12,7 +12,6 @@
 #
 import os
 import sys
-import sphinx_rtd_theme
 
 sys.path.insert(0, os.path.abspath('..'))
 import robomasterpy
@@ -65,3 +64,15 @@ html_theme_options = {
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = []
+
+
+# Doc __init__ as well
+# https://stackoverflow.com/questions/5599254/how-to-use-sphinxs-autodoc-to-document-a-classs-init-self-method
+def skip(app, what, name, obj, would_skip, options):
+    if name == '__init__':
+        return False
+    return would_skip
+
+
+def setup(app):
+    app.connect("autodoc-skip-member", skip)
