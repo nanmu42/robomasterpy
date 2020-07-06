bb6772d7e64a65e858136233940c23c3e443965c
nanmu42 <i@nanmu.me>
2020-07-06 11:48:05 +0800

    doc: partial doc in framework

--- robomasterpy/framework.py
+++ robomasterpy/framework.py
@@ -22,9 +22,51 @@ from .client import CTX, LOG_LEVEL, PUSH_PORT, GimbalAttitude, ChassisPosition, 
 
 
 class Worker:
+    """
+    用户逻辑的载体，继承这个类然后将你的逻辑写到work()方法中即可。
+    你的逻辑可以是有状态的或无状态的，如果需要，你可以在继承的新类中使用自定义的任意属性保存你的状态。
+    如果你需要打印日志，使用Worker.logger属性。
+    创建你的新类的实例后使用Hub.worker()注册到Hub实例供其调用。
+    一个Worker子类一般只做一件事情，多个Worker子类各司其职，相互协作，通过multiprocessing.Queue进行单向通讯，
+    最终，负责传感器的Worker的数据会汇聚到负责控制的Worker中，负责控制的Worker再使用Commander向机甲下令。
+
+    RoboMasterPy.framework中提供了多个定制化，开箱即用的Worker以满足
+    接收视频流（Vision）、接收事件（EventListener）和推送（PushListener）、汇聚信息控制机甲（Mind）等常见需求，
+    请参阅API文档中的“预置Worker”部分。
+
+    Worker takes user's business logic. Inherit this class and write logic code in work() method.
+    A worker can be stateful or stateless, at your choice. You may use some user-defined attributes to store your state if the need arises.
+    Use Worker.logger attribute if some logs need to be printed.
+    Register your well-defined new worker to hub using Hub.worker() so that hub schedule and calls your logic.
+    One Worker subclass nearly always does only one business. Multiple Worker subclasses do their own jobs and cooperate,
+    communicate through multiprocessing.Queue in one-way fashion.
+    Data from those subclasses in charge of sensor flows into subclass in charge of controlling, who command your Robomaster by Commander.
+
+    RoboMasterPy.framework provides many customized and out-of-box Worker to cover common usage like
+    receiving video stream(vision), receiving events(EventListener) & pushes(PushListener), gathering info and controlling the Robomaster(Mind), etc.
+    Please refer to "Sugared Workers" section in API doc.
+    """
+
     QUEUE_TIMEOUT: float = 0.05
 
     def __init__(self, name: str, out: Optional[mp.Queue], protocol: Optional[str], address: Tuple[str, int], timeout: Optional[float], loop: bool = True):
+        """
+        初始化Worker，这个方法一般在子类的__init__()方法中调用，不会直接使用。
+
+        Initialize Worker, called in its subclasses's __init__(), seldom used directly.
+
+        :param name: worker实例的名称，这个名称也会作为进程的名称，你应该使用一个有利于调试的，描述性好的名字。
+            name of Worker instance, which is also the name of the process. Choose wisely to benefit when debugging.
+        :param out: （可选）用于输出产物的multiprocessing.Queue，定义后Worker._outlet()方法可用。
+            (Optional) a multiprocessing.Queue to put worker's product. Worker._outlet() is available after this parameter is defined.
+        :param protocol: 连接机甲的协议名称，从tcp, udp和None中选择，在tcp和udp选项下Worker._intake()方法可用。
+            Protocol to use to connect your Robomaster, choose one in tcp, udp, None. Worker._intake() is available under tcp or udp.
+        :param address: 机甲的IP地址和端口号，可从Commander.get_ip()中获得机甲IP，端口号和业务有关，见framework.*_PORT.
+            IP and port to Robomaster. IP is available using Commander.get_ip(), and refer to framework.*_PORT for port number.
+        :param timeout: tcp或udp的连接超时。   timeout of tcp or udp.
+        :param loop: 是否在Worker的生命周期中循环调用work()方法，常见True，在方法提供自己的生命周期管理的时候可选False.
+            Whether call work() method in a loop for Worker's lifetime, usually True. Set to False when work() has its own lifecycle management.
+        """
         assert name is not None and name != '', 'choose a good name to make life easier'
 
         self._mu = CTX.Lock()
@@ -61,10 +103,22 @@ class Worker:
         self.close()
 
     @property
-    def closed(self):
+    def closed(self) -> bool:
+        """
+        当前Worker是否已经永久停止。
+
+        Whether the worker has stopped working eventually.
+
+        :return: True for stopped.
+        """
         return self._closed
 
     def close(self):
+        """
+        让Worker停止工作，本方法一般由Hub调用。
+
+        Let worker to stop. Nearly always called by Hub.
+        """
         with self._mu:
             if self.closed:
                 return
@@ -78,10 +132,28 @@ class Worker:
 
     @property
     def name(self) -> str:
+        """
+        当前Worker的名字。
+
+        The worker's name.
+        """
         return self._name
 
     def work(self) -> None:
-        raise SyntaxError('implement me')
+        """
+        在本方法中实现你的业务逻辑，你可能需要在这里使用下列方法和属性：
+
+        * 使用self._intake()方法从tcp或udp中获取数据；
+        * 使用self._outlet()方法将产物放到out中，注意，如果out被没有即时消费的产物填满，self._outlet()会丢弃最新的产物；
+        * 使用self.logger属性打印日志。
+
+        Implement your business logic in this method. These methods and attributes may be useful:
+
+        * use self._intake() method to intake data from tcp or udp connection;
+        * use self._outlet() to put product to ``out``. Keep in mind if ``out`` is filled up with unconsumed product, self._outlet() discards the latest products.
+        * use self.logger for log printing.
+        """
+        raise NotImplementedError('implement me')
 
     def __call__(self) -> None:
         try:
@@ -97,7 +169,12 @@ class Worker:
             self.close()
 
     @property
-    def logger(self):
+    def logger(self) -> logging.Logger:
+        """
+        使用本属性打印日志。
+
+        Use this attribute for logging.
+        """
         return self._logger
 
     def _assert_ready(self):
@@ -117,6 +194,13 @@ class Worker:
             break
 
     def get_address(self) -> Tuple[str, int]:
+        """
+        获取Worker连接的IP和port.
+
+        get IP and port which the worker currently connects to.
+
+        :return: (IP, port)
+        """
         return self._address
 
     def __enter__(self):
@@ -127,15 +211,39 @@ class Worker:
 
 
 class Hub:
+    """
+    程序中枢。
+
+    * 使用self.worker()注册你的Worker；
+    * 使用self.run()开始运行；
+    * 使用Ctrl + C停止程序。
+
+    Hub is the orchestrator.
+
+    * Use self.worker() to register your worker;
+    * Use self.run() to run;
+    * Use Ctrl + C for exiting.
+    """
+
     TERMINATION_TIMEOUT = 10
 
     def __init__(self):
+        """
+        创建Hub不需要提供参数。
+
+        Hub does not need parameters to initialize.
+        """
         self._mu = CTX.Lock()
         with self._mu:
             self._closed: bool = False
             self._workers: List = []
 
     def close(self):
+        """
+        让Hub以及Hub名下的所有Worker停止工作。这个方法不需要用户来调用。
+
+        Stop hub and workers registered under hub. This method does not need to be called by user.
+        """
         with self._mu:
             if self._closed:
                 return
@@ -150,7 +258,7 @@ class Hub:
                     worker.terminate()
 
     def _assert_ready(self):
-        assert not self._closed, 'EP is closed'
+        assert not self._closed, 'Hub is closed'
 
     def __enter__(self):
         return self
@@ -160,14 +268,20 @@ class Hub:
 
     def worker(self, worker_class, name: str, args: Tuple = (), kwargs=None):
         """
-        Register worker to process sensor data fetching, calculation,
-        inference,controlling, communication and more.
+        将worker注册到hub.
+        所有的worker都在独立的进程中运行。
+
+        Register worker to hub.
         All workers run in their own operating system process.
 
-        :param worker_class:
-        :param name:
-        :param args: args to func
-        :param kwargs: kwargs to func
+        :param worker_class: worker的类，注意不是worker实例。
+            class of worker to be registered, note provide the class, not a instance.
+        :param name: worker的名字，选个好名字可以让调试更容易。
+            worker's name. A good name makes debugging less painful.
+        :param args: 创建worker需要使用的参数。
+            args to initialize the worker.
+        :param kwargs: 创建worker需要使用的kwargs参数。
+            kwargs to initialize the worker.
         """
         if kwargs is None:
             kwargs = {}
@@ -182,10 +296,11 @@ class Hub:
 
     def run(self):
         """
-        Start workers and block the main process until
-        all workers terminate.
+        按注册顺序启动所有worker，阻塞主进程。
+        Hub在接收到SIGTERM或者SIGINT时会尝试安全退出。
 
-        Mind tries to shutdown itself gracefully when receiving
+        Start workers and block the main process.
+        Hub tries to shutdown itself gracefully when receiving
         SIGTERM or SIGINT.
         """
         with self._mu:
@@ -206,11 +321,22 @@ class Hub:
 
 
 class PushListener(Worker):
+    """
+    监听并解析机甲大师的推送，输出强类型的推送内容。
+
+    Listen and parse pushes from Robomaster, product parsed pushes in strong typed manner.
+    """
     PUSH_TYPE_CHASSIS: str = 'chassis'
     PUSH_TYPE_GIMBAL: str = 'gimbal'
     PUSH_TYPES: Tuple[str] = (PUSH_TYPE_CHASSIS, PUSH_TYPE_GIMBAL)
 
     def __init__(self, name: str, out: mp.Queue):
+        """
+
+
+        :param name:
+        :param out:
+        """
         super().__init__(name, out, 'udp', ('', PUSH_PORT), None)
 
     def _parse(self, msg: str) -> List:
